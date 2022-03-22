1602d582639672c91a72453a10401036b5a5455b
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2022-03-21 21:05:42 -0700

    leet code binary tree solved

---
+++ Binary_Tree Problems/InorderTraversal.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class InorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+     
+        return inorderTraversal(root, new LinkedList<>());
+  
+    }
+    
+    
+    public List<Integer> inorderTraversal(TreeNode root, List<Integer> list){
+        if(root != null){
+            if(root.left == null && root.right == null){
+                list.add(root.val);
+            }else{
+                list = inorderTraversal(root.left, list);
+                list.add(root.val);
+                list = inorderTraversal(root.right, list);
+            }
+        }
+        return list;
+    }
+    
+    
+ 
+    
+     
+}
