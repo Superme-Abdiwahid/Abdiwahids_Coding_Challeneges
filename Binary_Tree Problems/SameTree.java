d84477130aada62d20c3ac9c5408a6f7590837f7
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2022-03-22 06:05:03 -0700

    another challenege solved

---
+++ Binary_Tree Problems/SameTree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/* Given the roots of two binary trees p and q, write a function to check if 
+    they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value. */
+class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+       return  isSameTreeHelper(p, q);
+    }
+    
+    public boolean isSameTreeHelper(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }else if(p == null && q != null || q == null && p != null){
+            return false;
+        }else if(q.val != p.val){
+            return false;
+        }
+            return isSameTreeHelper(p.left, q.left) && isSameTreeHelper(p.right, q.right);
+    }
+}
