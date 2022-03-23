404fdd6df835dd377fd53808aaa175e2ef97526c
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2022-03-23 13:41:33 -0700

    update run time using string builder

--- String Problems/ReverseInteger.java
+++ String Problems/ReverseInteger.java
@@ -3,7 +3,7 @@ class ReverseInteger {
     
     
     //Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside // the signed 32-bit integer range [-231, 231 - 1], then return 0.
-    public  int reverse(int x){
+    public  int reverse(int x)throws StringIndexOutOfBoundsException{
       StringBuilder builder = new StringBuilder();
       int result = 0;
       if(x >= Integer.MAX_VALUE || x <= Integer.MIN_VALUE){
